bb95a3a6c205b4df5b223f1a86eb360782b7f372
Andres Visco Bonomo <andres.visco@softtek.com>
2018-09-13 14:35:20 -0300

    Completada

--- ANVI6NoanetRegistroDePersonal/Controller/SincronizarAzure.cs
+++ ANVI6NoanetRegistroDePersonal/Controller/SincronizarAzure.cs
@@ -9,13 +9,43 @@ using Windows.Graphics.Imaging;
 using System.IO;
 using Microsoft.ProjectOxford.Face;
 using System.Collections.ObjectModel;
+using ANVI6NoanetRegistroDePersonal.Views;
+using static ANVI6NoanetRegistroDePersonal.Views.SincronizarFichaje;
+using Windows.Foundation;
+using Windows.Storage.Streams;
 
 namespace ANVI6NoanetRegistroDePersonal.Controller
 {
     public class SincronizarAzure
     {
+        public ObservableCollection<Empleado> empleadosObS = new ObservableCollection<Empleado>();
+        public static async Task<SoftwareBitmap> CreateFromBitmap(SoftwareBitmap softwareBitmap, uint width, uint heigth)
+        {
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
 
-        public async Task<bool> ObtenerIdentidades(ObservableCollection<FotosCapttuple> listadoCaras)
+
+                encoder.SetSoftwareBitmap(softwareBitmap);
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Linear;
+
+
+
+                var ancho = width * (0.2);
+                var alto = heigth * (0.2);
+
+
+                encoder.BitmapTransform.ScaledWidth = (uint)ancho;
+                encoder.BitmapTransform.ScaledHeight = (uint)alto;
+
+                await encoder.FlushAsync();
+
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                return await decoder.GetSoftwareBitmapAsync(softwareBitmap.BitmapPixelFormat, softwareBitmap.BitmapAlphaMode);
+            }
+        }
+        public async Task<ObservableCollection<Empleado>> ObtenerIdentidades(ObservableCollection<FotosCapttuple> listadoCaras)
         {
             string subscriptionKey = "5ff19b57095a4d10bf64274ed9e6ef30";
             string subscriptionEndpoint = "https://southcentralus.api.cognitive.microsoft.com/face/v1.0";
@@ -24,32 +54,33 @@ namespace ANVI6NoanetRegistroDePersonal.Controller
             byte[] arrayByteData;
             foreach (var item in listadoCaras)
             {
+                var empleado = new Empleado();
+
                 SoftwareBitmap softwareBitmap = item.TupleFotosCapturadas.Item4;
-                arrayByteData = await EncodedBytesClass.EncodedBytes(softwareBitmap, BitmapEncoder.BmpEncoderId);
+                SoftwareBitmap softwareBitmapCropped = await CreateFromBitmap(softwareBitmap, (uint)softwareBitmap.PixelWidth, (uint)softwareBitmap.PixelHeight);
+                SoftwareBitmap displayableImage = SoftwareBitmap.Convert(softwareBitmapCropped, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+
+                arrayByteData = await EncodedBytesClass.EncodedBytes(displayableImage, BitmapEncoder.JpegEncoderId);
                 var nuevoStreamFace = new MemoryStream(arrayByteData);
 
                 try
                 {
-                    IEnumerable<FaceAttributeType> faceAttributes =
-                        new FaceAttributeType[] {
-                            FaceAttributeType.Gender,
-                            FaceAttributeType.Age,
-                            FaceAttributeType.Smile,
-                            FaceAttributeType.Emotion,
-                            FaceAttributeType.Glasses,
-                            FaceAttributeType.Hair };
-                    var faces = await faceServiceClient.DetectAsync(nuevoStreamFace, true, false, faceAttributes);
+                   
+                    var faces = await faceServiceClient.DetectAsync(nuevoStreamFace, true, false);
 
                     var resultadoIdentifiacion = await faceServiceClient.IdentifyAsync(faces.Select(ff => ff.FaceId).ToArray(), largePersonGroupId: "1");
 
-                    for (int i = 0; i < faces.Length; i++)
-                    {
-                        var res = resultadoIdentifiacion[i];
+                        var res = resultadoIdentifiacion[0];
 
                         if(res.Candidates.Length > 0)
                         {
                             var nombrePersona = await faceServiceClient.GetPersonInLargePersonGroupAsync("1", res.Candidates[0].PersonId);
+                            empleado.EmpleadoNombe = nombrePersona.Name.ToString();
+                            empleado.Horario = item.TupleFotosCapturadas.Item2.ToString();
+
                             
+                        
 
                             //var estadoAnimo = 
 
@@ -58,16 +89,18 @@ namespace ANVI6NoanetRegistroDePersonal.Controller
                         {
                             //txtResult.Text = "Unknown";
                         }
-                    }
+                    
                 }
                 catch (Exception ex)
                 {
                     var error = ex.Message.ToString();
                 }
+                empleadosObS.Add(empleado);
+
             }
 
 
-            return true;
+            return empleadosObS;
 
         }
 
--- ANVI6NoanetRegistroDePersonal/Views/SincronizarFichaje.xaml.cs
+++ ANVI6NoanetRegistroDePersonal/Views/SincronizarFichaje.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using ANVI6NoanetRegistroDePersonal.Views;
 using ANVI6NoanetRegistroDePersonal.Controller;
 using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Automation.Peers;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,14 +24,16 @@ namespace ANVI6NoanetRegistroDePersonal.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class SincronizarFichaje : Page
     {
+        public ObservableCollection<Empleado> empleadosLista = new ObservableCollection<Empleado>();
         public ObservableCollection<FotosCapttuple> fotosCapttuplesLista = new ObservableCollection<FotosCapttuple>();
         public ObservableCollection<Tuple<FotosCapttuple>> tuplesViewSinc = new ObservableCollection<Tuple<FotosCapttuple>>();
         public SincronizarFichaje()
         {
+            
             this.InitializeComponent();
-
             Views.IniciarFichaje iniciarFichaje = new IniciarFichaje();
             fotosCapttuplesLista = iniciarFichaje.tuplesDevolver().Result;
             foreach (var item in fotosCapttuplesLista)
@@ -48,10 +51,73 @@ namespace ANVI6NoanetRegistroDePersonal.Views
 
         private async void btnSincronizar_Click(object sender, RoutedEventArgs e)
         {
+            empleadosLista.Clear();
             SincronizarAzure sincronizarAzure = new SincronizarAzure();
-            await sincronizarAzure.ObtenerIdentidades(fotosCapttuplesLista);
+            NotifyUser("Iniciando Sincronización", NotifyType.StatusMessage);
+            var empleadosListaObservable = await sincronizarAzure.ObtenerIdentidades(fotosCapttuplesLista);
+            empleadosLista = empleadosListaObservable;
+            lstEmpleadosFichados.ItemsSource = empleadosLista;
+            NotifyUser("Sincronización Finalizada", NotifyType.StatusMessage);
+            tuplesViewSinc.Clear();
 
 
         }
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            // If called from the UI thread, then update immediately.
+            // Otherwise, schedule a task on the UI thread to perform the update.
+
+            UpdateStatus(strMessage, type);
+
+        }
+        public enum NotifyType
+        {
+            StatusMessage,
+            ErrorMessage
+        };
+
+        public void UpdateStatus(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    break;
+            }
+
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusPanel.Visibility = Visibility.Collapsed;
+            }
+
+            // Raise an event if necessary to enable a screen reader to announce the status update.
+            var peer = FrameworkElementAutomationPeer.FromElement(StatusBlock);
+            if (peer != null)
+            {
+                peer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+            }
+        }
+    
+        public class Empleado
+        {
+            public string EmpleadoNombe { get; set; }
+            public string Horario { get; set; }
+            
+                
+        }
     }
+    
 }
